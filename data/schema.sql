c099e064b43f6093e35f4e2f8584a063ae0a0c13
panda <suntianwei@msn.com>
2013-03-13 01:21:52 +0800

    modify sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,3 +1,20 @@
+-- MySQL dump 10.13  Distrib 5.6.7-rc, for osx10.7 (i386)
+--
+-- Host: localhost    Database: GoCook
+-- ------------------------------------------------------
+-- Server version	5.6.7-rc
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 --
 -- Current Database: `GoCook`
 --
@@ -7,35 +24,385 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `GoCook` /*!40100 DEFAULT CHARACTER SET
 USE `GoCook`;
 
 --
+-- Table structure for table `dish`
+--
+
+DROP TABLE IF EXISTS `dish`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dish` (
+  `dish_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `recipe_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `content` text,
+  `photo_id` int(11) DEFAULT NULL,
+  `favor_count` int(11) DEFAULT NULL,
+  `state` char(1) DEFAULT NULL,
+  PRIMARY KEY (`dish_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dish`
+--
+
+LOCK TABLES `dish` WRITE;
+/*!40000 ALTER TABLE `dish` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dish` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dish_comment`
+--
+
+DROP TABLE IF EXISTS `dish_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dish_comment` (
+  `comment_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `dish_id` int(11) DEFAULT NULL,
+  `recipe_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `content` text,
+  `state` char(1) DEFAULT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dish_comment`
+--
+
+LOCK TABLES `dish_comment` WRITE;
+/*!40000 ALTER TABLE `dish_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dish_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `material` (
+  `material_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` int(11) DEFAULT NULL,
+  `catgory` int(11) DEFAULT NULL,
+  PRIMARY KEY (`material_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `photo`
+--
+
+DROP TABLE IF EXISTS `photo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `photo` (
+  `photo_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `recipe_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `upload_time` datetime DEFAULT NULL,
+  `image_name` varchar(100) DEFAULT NULL,
+  `image_path` varchar(100) DEFAULT NULL,
+  `thumb_path` varchar(100) DEFAULT NULL,
+  `desc` varchar(100) DEFAULT NULL COMMENT '暂时无用',
+  `refer_count` int(11) DEFAULT NULL COMMENT '引用数，暂时无用',
+  PRIMARY KEY (`photo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `photo`
+--
+
+LOCK TABLES `photo` WRITE;
+/*!40000 ALTER TABLE `photo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `photo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `portrait`
+--
+
+DROP TABLE IF EXISTS `portrait`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `portrait` (
+  `portrait_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(100) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`portrait_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `portrait`
+--
+
+LOCK TABLES `portrait` WRITE;
+/*!40000 ALTER TABLE `portrait` DISABLE KEYS */;
+/*!40000 ALTER TABLE `portrait` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipe`
+--
+
+DROP TABLE IF EXISTS `recipe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipe` (
+  `recipe_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `desc` text,
+  `collected_count` int(11) DEFAULT NULL,
+  `dish_count` int(11) DEFAULT NULL,
+  `comment_count` int(11) DEFAULT NULL,
+  `browse_count` int(11) DEFAULT NULL,
+  `catgory` varchar(100) DEFAULT NULL,
+  `cover_img_id` bigint(11) DEFAULT NULL,
+  `materials` text,
+  `recipe_steps` text,
+  `tips` text,
+  PRIMARY KEY (`recipe_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipe`
+--
+
+LOCK TABLES `recipe` WRITE;
+/*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipe_comment`
+--
+
+DROP TABLE IF EXISTS `recipe_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipe_comment` (
+  `comment_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `recipe_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `content` text,
+  `state` char(1) DEFAULT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipe_comment`
+--
+
+LOCK TABLES `recipe_comment` WRITE;
+/*!40000 ALTER TABLE `recipe_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recipe_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT '',
   `display_name` varchar(50) DEFAULT NULL,
+  `portrait` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT '',
   `password` varchar(128) NOT NULL,
   `qq_openid` varchar(255) DEFAULT NULL,
-  `qq_access_token` varchar(255) DEFAULT NULL,
-  `weibo_id` varchar(255) DEFAULT NULL,
+  `qq_access_token` varchar(255) DEFAULT '',
+  `weibo_id` bigint(11) DEFAULT NULL,
   `weibo_access_token` varchar(255) DEFAULT NULL,
   `state` smallint(6) DEFAULT NULL,
-  `test_field` int(11) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `age` smallint(6) DEFAULT NULL,
+  `career` varchar(100) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `user_type` char(1) DEFAULT NULL,
+  `register_time` datetime DEFAULT NULL,
+  `intro` text,
   PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`),
   UNIQUE KEY `qq_openid` (`qq_openid`),
   UNIQUE KEY `weibo_id` (`weibo_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-
--- INSERT INTO `user` (`user_id`, `username`, `email`, `display_name`, `password`, `state`, `user_type`)
--- VALUES
--- 	(1,NULL,'aa@aa.com',NULL,'$2y$14$IMWe.Hiz9KpK2OqitxF6LOxNR6Vg3oGk/gzzXkq6kRU1R/3/DTjuW',NULL,NULL);
-
+INSERT INTO `user` VALUES (1,NULL,NULL,NULL,'a@a.com','$2y$14$ndvisBHZ.P5CHAYemXaK7.n/Bbs/Sgoc/mITLmzPrhOjEvS0dyJiC',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0000-00-00 00:00:00',NULL),(2,NULL,NULL,NULL,'aa@aa.com','$2y$14$LqwOWxyPYLSuGB2TcbOYXu/mZu/BylGgIfT83JhI5w7olXQVAcHoO',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0000-00-00 00:00:00',NULL),(3,NULL,NULL,NULL,'a@aa.com','$2y$14$AWPCFljdG3rSl.00dKM6qOCEfHyYvSaDtcsRJSSNnEIsgj0rZzr82',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0000-00-00 00:00:00',NULL),(4,NULL,NULL,NULL,'a@aaa.com','$2y$14$sERvvSj2JnmeZrZ5F6Pgi.s5yxE8kkVWkbHKxZ4uJAcoSs5FsFQZu',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0000-00-00 00:00:00',NULL),(5,NULL,NULL,NULL,'aaa@aaa.com','$2y$14$.5SkY18BmK2MomJqy2aMo.yd.SqADplGMsLog7uqAsMZz897tY1Km',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0000-00-00 00:00:00',NULL),(6,NULL,NULL,NULL,'aaa@aaaa.com','$2y$14$PSNq8X4k08yJvxfZpcArtOcKFqyoLt1hF1odG.KvjML75RF9ApVjy',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0000-00-00 00:00:00',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user_collection`
+--
+
+DROP TABLE IF EXISTS `user_collection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_collection` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `recipe_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_collection`
+--
+
+LOCK TABLES `user_collection` WRITE;
+/*!40000 ALTER TABLE `user_collection` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_collection` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_comment`
+--
+
+DROP TABLE IF EXISTS `user_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_comment` (
+  `comment_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `content` text,
+  `state` char(1) DEFAULT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_comment`
+--
+
+LOCK TABLES `user_comment` WRITE;
+/*!40000 ALTER TABLE `user_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_favor`
+--
+
+DROP TABLE IF EXISTS `user_favor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_favor` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `dish_id` int(11) DEFAULT NULL,
+  `state` char(1) DEFAULT NULL COMMENT '当为0时表示未赞',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_favor`
+--
+
+LOCK TABLES `user_favor` WRITE;
+/*!40000 ALTER TABLE `user_favor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_favor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_info` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `collect_count` int(11) DEFAULT NULL,
+  `dish_count` int(11) DEFAULT NULL,
+  `recipe_count` int(11) DEFAULT NULL,
+  `following_count` int(11) DEFAULT NULL,
+  `followed_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_info`
+--
+
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_relation`
+--
+
+DROP TABLE IF EXISTS `user_relation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_relation` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `target_id` int(11) DEFAULT NULL,
+  `state` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_relation`
+--
+
+LOCK TABLES `user_relation` WRITE;
+/*!40000 ALTER TABLE `user_relation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_relation` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-03-13  1:20:02
