bdf6b6a2858ae1f3b839715f5b3e6d4a7c7ff98b
panda <suntianwei@msn.com>
2013-04-14 14:53:55 +0800

    modify recipe repository

--- module/Main/src/Main/Repository/RecipeRepository.php
+++ module/Main/src/Main/Repository/RecipeRepository.php
@@ -12,7 +12,29 @@ class RecipeRepository extends EntityRepository
         $qb->select('r')
             ->from('Main\Entity\Recipe', 'r')
             ->where('r.catgory LIKE :cat')
-            ->setParameter('cat', '%'.$catgory.'%')
+            ->setParameter('cat', '%'.$catgory.'%');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getRecipesByCollectedCount()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('r')
+            ->from('Main\Entity\Recipe', 'r')
+            ->add('orderBy', 'r.collected_count DESC')
+            ->setMaxResults(100);//取前100个
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getRecipesByCreateDate()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('r')
+            ->from('Main\Entity\Recipe', 'r')
+            ->add('orderBy', 'r.create_time DESC')
+            ->setMaxResults(100);//取前100个
 
         return $qb->getQuery()->getResult();
     }
